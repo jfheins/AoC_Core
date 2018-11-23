cfd0c5c0fa2d4fcf687c17a974360dacd95397a9
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-23 11:29:56 +0100

    Create core package

---
+++ BreadthFirstSearch.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core
+{
+	public class BreadthFirstSearch<TNode, TEdge>
+	{
+		public delegate void ProgressReporterCallback(int workingSetCount, int visitedCount);
+
+		private readonly IEqualityComparer<NodeWithPredecessor> _comparer;
+		private readonly Func<TNode, IEnumerable<TNode>> _expander;
+
+		/// <summary>
+		///     Prepares a breadth first search.
+		/// </summary>
+		/// <param name="comparer">Comparison function that determines node equality</param>
+		/// <param name="expander">Callback to get the possible edges</param>
+		/// <param name="combiner">Callback to combine a source node and an edge to a (possibly new) node. May return null.</param>
+		public BreadthFirstSearch(IEqualityComparer<TNode> comparer,
+			Func<TNode, IEnumerable<TEdge>> expander,
+			Func<TNode, TEdge, TNode> combiner)
+		{
+			_comparer = new NodeComparer(comparer);
+			_expander = node => expander(node).Select(edge => combiner(node, edge)).Where(x => x != null);
+		}
+
+		/// <summary>
+		///     Prepares a breadth first search.
+		/// </summary>
+		/// <param name="comparer">Comparison function that determines node equality</param>
+		/// <param name="expander">Callback to get the possible nodes from a source node</param>
+		public BreadthFirstSearch(IEqualityComparer<TNode> comparer, Func<TNode, IEnumerable<TNode>> expander)
+		{
+			_comparer = new NodeComparer(comparer);
+			_expander = expander;
+		}
+
+		public TNode[] Search(TNode initialNode,
+			Func<TNode, bool> targetPredicate,
+			ProgressReporterCallback progressReporter = null)
+		{
+			var visitedNodes = new HashSet<NodeWithPredecessor>(_comparer);
+			var nextNodes = new HashSet<NodeWithPredecessor>(_comparer) {new NodeWithPredecessor(initialNode)};
+
+			if (targetPredicate(initialNode))
+				return new[] { initialNode };
+
+			while (nextNodes.Count > 0)
+			{
+				progressReporter?.Invoke(visitedNodes.Count, nextNodes.Count);
+
+				visitedNodes.UnionWith(nextNodes);
+
+				var expanded = nextNodes.AsParallel()
+					.SelectMany(sourceNode => _expander(sourceNode.Current)
+						.Select(dest => new NodeWithPredecessor(dest, sourceNode))
+						.Where(dest => !visitedNodes.Contains(dest)));
+
+				nextNodes = new HashSet<NodeWithPredecessor>(expanded, _comparer);
+
+				foreach (var node in nextNodes)
+					if (targetPredicate(node.Current))
+						return node.GetHistory().ToArray();
+			}
+
+			return null;
+		}
+
+		private class NodeComparer : EqualityComparer<NodeWithPredecessor>
+		{
+			private readonly IEqualityComparer<TNode> _comparer;
+
+			public NodeComparer(IEqualityComparer<TNode> comparer)
+			{
+				_comparer = comparer;
+			}
+
+			public override bool Equals(NodeWithPredecessor a, NodeWithPredecessor b)
+			{
+				return _comparer.Equals(a.Current, b.Current);
+			}
+
+			public override int GetHashCode(NodeWithPredecessor x)
+			{
+				return _comparer.GetHashCode(x.Current);
+			}
+		}
+
+		private class NodeWithPredecessor
+		{
+			public NodeWithPredecessor(TNode current, NodeWithPredecessor predecessor = null)
+			{
+				Predecessor = predecessor;
+				Current = current;
+			}
+
+			public TNode Current { get; }
+			private NodeWithPredecessor Predecessor { get; }
+
+			public IEnumerable<TNode> GetHistory()
+			{
+				var pointer = this;
+				do
+				{
+					yield return pointer.Current;
+					pointer = pointer.Predecessor;
+				} while (pointer != null);
+			}
+		}
+	}
+}
---
+++ LinqHelpers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public static class LinqHelpers
+    {
+        /// <summary>
+        ///    Verpackt den angegebenen Wert in eine Enumeration mit einem Element.
+        /// </summary>
+        /// <typeparam name="T">Ein beliebiger Typ.</typeparam>
+        /// <param name="item">Der Wert, der verpackt werden soll.</param>
+        /// <returns>Eine Enumeration, die genau einen Wert enthält.</returns>
+        public static IEnumerable<T> ToEnumerable<T>(this T item)
+        {
+            yield return item;
+        }
+
+        public static IEnumerable<int> IndexWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return source.Select((value, index) => new { value, index })
+                .Where(x => predicate(x.value))
+                .Select(x => x.index);
+        }
+
+        /// <summary>
+        ///    Liefert zu einer Enumeration alle Paare zurück. Eine Enumeration mit n Elementen hat genau n-1 Paare.
+        ///    Die Quelle wird nur einmal durchlaufen. Für jedes Paar wird ein neues Tupel generiert.
+        ///    Item1 ist stets das Element, dass in der Quelle zuerst vorkommt.
+        /// </summary>
+        /// <param name="source">Die Quelle, die paarweise enumeriert werden soll.</param>
+        /// <returns>
+        ///    Eine Enumeration mit n-1 überschneidenden Tupeln. Gibt eine leere Enumeration zurück, wenn die Quelle aus
+        ///    weniger als zwei Elmenten besteht.
+        /// </returns>
+        public static IEnumerable<Tuple<T, T>> PairwiseWithOverlap<T>(this IEnumerable<T> source)
+        {
+            using (var it = source.GetEnumerator())
+            {
+                if (!it.MoveNext())
+                    yield break;
+
+                var previous = it.Current;
+
+                while (it.MoveNext())
+                    yield return Tuple.Create(previous, previous = it.Current);
+            }
+        }
+
+        public static IEnumerable<Tuple<T, T>> Pairwise<T>(this IEnumerable<T> source)
+        {
+            var isPair = false;
+            T tempItem = default(T);
+            foreach (var item in source)
+            {
+                if (isPair)
+                {
+                    yield return Tuple.Create(tempItem, item);
+                    isPair = false;
+                }
+                else
+                {
+                    tempItem = item;
+                    isPair = true;
+                }
+            }
+        }
+
+		// https://stackoverflow.com/questions/419019/split-list-into-sublists-with-linq/20953521#20953521
+		public static IEnumerable<IEnumerable<T>> Chunks<T>(this IEnumerable<T> enumerable,
+			int chunkSize)
+		{
+			if (chunkSize < 1) throw new ArgumentException("chunkSize must be positive");
+
+			using (var e = enumerable.GetEnumerator())
+				while (e.MoveNext())
+				{
+					var remaining = chunkSize;    // elements remaining in the current chunk
+					var innerMoveNext = new Func<bool>(() => --remaining > 0 && e.MoveNext());
+
+					yield return e.GetChunk(innerMoveNext);
+					while (innerMoveNext()) {/* discard elements skipped by inner iterator */}
+				}
+		}
+
+		private static IEnumerable<T> GetChunk<T>(this IEnumerator<T> e,
+			Func<bool> innerMoveNext)
+		{
+			do yield return e.Current;
+			while (innerMoveNext());
+		}
+    }
+}
