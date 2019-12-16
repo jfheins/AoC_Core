4d7ddd1109de5218aa79487530ac7547764342ac
Julius F. Heins <jfheins@gmail.com>
2019-12-16 07:01:38 +0100

    Day 16 part 1

--- LinqHelpers.cs
+++ LinqHelpers.cs
@@ -295,5 +295,17 @@ namespace Core
             }
             return index;
         }
+
+        public static IEnumerable<T> RepeatIndefinitely<T>(this IEnumerable<T> source)
+        {
+            var list = source.ToList();
+            while (true)
+            {
+                foreach (var item in list)
+                {
+                    yield return item;
+                }
+            }
+        }
     }
 }
