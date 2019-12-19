80334184ba5d4008a3d99c3b32391e1142e7fac6
Julius F. Heins <jfheins@gmail.com>
2019-12-19 22:43:31 +0100

    work in progress

---
+++ BinarySearchInt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Core
+{
+    public class BinarySearchInt
+    {
+        private readonly Func<int, bool> _predicate;
+
+        /// <summary>
+        /// Searches an intercal via binary search.
+        /// The class offers methods to find the first value or the last value still matching.
+        /// </summary>
+        /// <param name="predicate"></param>
+        public BinarySearchInt(Func<int, bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public int FindFirst(int start = 0) => SearchOpenInterval(start, true).upper;
+        public int FindLast(int start = 0) => SearchOpenInterval(start, false).lower;
+
+        private (int lower, int upper) SearchOpenInterval(int lowerLimit, bool searchValue)
+        {
+            if (_predicate(lowerLimit) == searchValue)
+                throw new ArgumentOutOfRangeException(nameof(lowerLimit), "The lowerLimit should not fulfil the seach condition!");
+
+            var stepsize = 2;
+            while (_predicate(lowerLimit + stepsize) != searchValue)
+                stepsize *= 2;
+
+            return SearchInterval(lowerLimit + (stepsize / 2), lowerLimit + stepsize, searchValue);
+        }
+
+        // lower: known false, upper: known true
+        private (int lower, int upper) SearchInterval(int lowerLimit, int upperLimit, bool searchValue)
+        {
+            while (upperLimit - lowerLimit > 1)
+            {
+                var probeindex = lowerLimit + (upperLimit - lowerLimit) / 2;
+                if (_predicate(probeindex) == searchValue)
+                    upperLimit = probeindex;
+                else
+                    lowerLimit = probeindex;
+            }
+            return (lowerLimit, upperLimit);
+        }
+    }
+}
---
+++ BinarySearchLong.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class BinarySearchLong
+    {
+        private readonly Func<long, bool> _predicate;
+
+        /// <summary>
+        /// Searches an intercal via binary search.
+        /// The class offers methods to find the first value or the last value still matching.
+        /// </summary>
+        /// <param name="predicate"></param>
+        public BinarySearchLong(Func<long, bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public long FindFirst(long start = 0) => SearchOpenInterval(start, true).upper;
+        public long FindLast(long start = 0) => SearchOpenInterval(start, false).lower;
+
+        private (long lower, long upper) SearchOpenInterval(long lowerLimit, bool searchValue)
+        {
+            if (_predicate(lowerLimit) == searchValue)
+                throw new ArgumentOutOfRangeException(nameof(lowerLimit), "The lowerLimit should not fulfil the seach condition!");
+
+            var stepsize = 2;
+            while (_predicate(lowerLimit + stepsize) != searchValue)
+                stepsize *= 2;
+
+            return SearchInterval(lowerLimit + (stepsize / 2), lowerLimit + stepsize, searchValue);
+        }
+
+        // lower: known false, upper: known true
+        private (long lower, long upper) SearchInterval(long lowerLimit, long upperLimit, bool searchValue)
+        {
+            while (upperLimit - lowerLimit > 1)
+            {
+                var probeindex = lowerLimit + (upperLimit - lowerLimit) / 2;
+                if (_predicate(probeindex) == searchValue)
+                    upperLimit = probeindex;
+                else
+                    lowerLimit = probeindex;
+            }
+            return (lowerLimit, upperLimit);
+        }
+    }
+}
