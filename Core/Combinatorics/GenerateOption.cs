2510e7f85eb1a4e18429e147c750ea980967c4d4
Julius F. Heins <jfheins@gmail.com>
2020-11-30 13:34:54 +0100

    Formatting

--- Core/Combinatorics/GenerateOption.cs
+++ Core/Combinatorics/GenerateOption.cs
@@ -3,20 +3,20 @@
 
 namespace Core.Combinatorics
 {
-	/// <summary>
-	///     Indicates whether a Permutation, Combination or Variation meta-collections
-	///     generate repetition sets.
-	/// </summary>
-	public enum GenerateOption
-	{
-		/// <summary>
-		///     Do not generate additional sets, typical implementation.
-		/// </summary>
-		WithoutRepetition,
+    /// <summary>
+    ///     Indicates whether a Permutation, Combination or Variation meta-collections
+    ///     generate repetition sets.
+    /// </summary>
+    public enum GenerateOption
+    {
+        /// <summary>
+        ///     Do not generate additional sets, typical implementation.
+        /// </summary>
+        WithoutRepetition,
 
-		/// <summary>
-		///     Generate additional sets even if repetition is required.
-		/// </summary>
-		WithRepetition
-	}
+        /// <summary>
+        ///     Generate additional sets even if repetition is required.
+        /// </summary>
+        WithRepetition
+    }
 }
